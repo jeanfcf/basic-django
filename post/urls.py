4c33416c0adfb2212e41589125ea8774ebcdde02
Jean Coelho <felipe_jean80@hotmail.com>
2020-02-05 21:49:03 -0300

    Posts ajeitados

--- post/urls.py
+++ post/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from post.views import PostListView
 
 app_name = 'post'
 
 urlpatterns = [
-
+    path('posts',PostListView.as_view(),name="lista"),
 ]
--- post/views.py
+++ post/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from post.models import Post
+from django.views.generic import ListView
 # Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'Post/posts.html'
